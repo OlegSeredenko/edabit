d23dcb92a0ff719b0ed6ee823fa62be9c528eb6c
Oleg <80279502+OlegSeredenko@users.noreply.github.com>
2023-03-21 22:19:09 +0300

    Add files via upload

---
+++ switchDigit.js
@@ -0,0 +1,36 @@
+/*Переменная n хранит целое число от 0 до 9. Используя оператор switch, написать скрипт, 
+который в зависимости от числа будет выводить слово (Например, если n равно 3, то будет выводиться слово «три»)кратные 10
+*/
+let n = 1;
+switch (n) {
+  case 1:
+    alert( 'один' );
+    break;
+  case 2:
+    alert( 'два' );
+    break;
+  case 3:
+    alert( 'три' );
+    break;
+  case 4:
+    alert( 'четыре' );
+    break;
+  case 5:
+    alert( 'пять' );
+    break;
+  case 6:
+    alert( 'шесть' );
+    break;
+  case 7:
+    alert( 'семь' );
+    break;
+  case 8:
+    alert( 'восемь' );
+    break;
+  case 9:
+    alert( 'девять' );
+    break;
+  case 0:
+    alert( 'ноль' );
+    break;
+}
