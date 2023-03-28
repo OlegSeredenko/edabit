2bc2de4ea3adf8fa0acae0e29f22437b0b3bd754
Oleg <80279502+OlegSeredenko@users.noreply.github.com>
2023-03-28 22:57:25 +0300

    Add files via upload

---
+++ positiveNumbers.js
@@ -0,0 +1,13 @@
+/*напишите функцию, которая выводит положительные целые числа от 1 до 9
+*/
+
+function positiveNumbers()
+{
+    alert('старт');
+    for (let i = 1; i < 10; i++) {
+        alert(i);
+    }
+    alert('финиш');
+}
+
+console.log(positiveNumbers());
